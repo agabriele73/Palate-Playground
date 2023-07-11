7eb14cfeb1f0aa7878e7b77572604d2b46c7f8ab
agabriele73 <agabrielejr@gmail.com>
2023-07-10 19:26:54 -0700

    added css to login and signup

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -11,6 +11,18 @@ function LoginFormModal() {
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
+  // useEffect(() => {
+  //   initializeGoogleSignIn();
+  // }, []);
+
+  // const initializeGoogleSignIn = () => {
+  //   window.gapi.load("auth2", () => {
+  //     window.gapi.auth2.init({
+  //       client_id: "848629788977-t77doramjd5a2u3pfq80kbsvr659m1j9.apps.googleusercontent.com",
+  //     });
+  //   });
+  // }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
@@ -20,9 +32,30 @@ function LoginFormModal() {
         closeModal()
     }
   };
+  
+  // const handleGoogleSignIn = () => {
+  //   window.gapi.auth2
+  //     .getAuthInstance()
+  //     .signIn()
+  //     .then((googleUser) => {
+  //       const profile = googleUser.getBasicProfile();
+  //       const idToken = googleUser.getAuthResponse().id_token;
+  //       const email = profile.getEmail();
+
+  //       const data = dispatch(login(idToken));
+  //       if (data) {
+  //         setErrors(data);
+  //       } else {
+  //         closeModal();
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error signing in with Google:", error);
+  //     });
+  // };
 
   return (
-    <>
+    <div className="login-form">
       <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
         <ul>
@@ -50,7 +83,8 @@ function LoginFormModal() {
         </label>
         <button type="submit">Log In</button>
       </form>
-    </>
+        {/* <div class="g-signin2" data-onsuccess="handleGoogleSignIn"><button onClick={handleGoogleSignIn}>Sign In with Google</button></div> */}
+    </ div>
   );
 }
 
