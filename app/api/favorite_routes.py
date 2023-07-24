f12935e35e8664c8c007847438a787044cfa8eb7
agabriele73 <agabrielejr@gmail.com>
2023-07-24 15:05:27 -0700

    added delte favorites

--- app/api/favorite_routes.py
+++ app/api/favorite_routes.py
@@ -37,3 +37,18 @@ def get_user_favorites():
         favorite_data.append(favorite_dict)
     
     return jsonify({"Favorites": favorite_data})
+
+
+@favorite_routes.route('/<int:favorite_id>', methods=['DELETE'])
+@login_required
+def delete_favorite(favorite_id):
+    favorite = Favorite.query.get(favorite_id)
+    if not favorite:
+        return jsonify({'message': 'Favorite not found'}), 404
+    
+    if favorite.user_id != current_user.id:
+        return jsonify({'message': 'You do not own this favorite'}), 401
+    
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({'message': 'Successfully deleted'}), 200
